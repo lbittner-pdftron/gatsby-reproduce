3e94e712d20b8286b2e45fadc10fb2773687e57e
Logan Bittner <lbittner@pdftron.com>
2019-07-10 13:47:28 -0700

    Add more config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,66 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-layout`,
       options: {
           component: require.resolve(`./src/layouts/MainLayout.js`)
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/chunks`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // `gatsby-remark-autolink-headers`,
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+              ignoreFileExtensions: []
+            }
+          },
+          {
+            resolve: `gatsby-remark-external-links`,
+            options: {
+              target: `_blank`
+            }
+          },
+        ]
+      }
+    },
+    `gatsby-transformer-json`,
+    `gatsby-transformer-yaml`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `navs`,
+        path: `${__dirname}/src/navs`,
+      },
+    },
   ],
+  pathPrefix: `/`
 }
